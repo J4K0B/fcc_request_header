42eab00014cef0c07d0c4cefef75e8b5829d601d
Jakob <jakob.tepe@gmail.com>
2017-02-17 15:42:42 +0000

    finished project functionality

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var ip;
+var lang;
+var os;
+var result;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  var regExp = /\(([^)]+)\)/;
+  ip = req.headers["x-forwarded-for"];
+  lang = req.headers["accept-language"].split(",")[0];
+  os = regExp.exec(req.headers["user-agent"])[1];
+  result = {
+      "ipaddress": ip,
+      "language": lang,
+      "software": os
+  };
+  res.send(JSON.stringify(result));
+  
+});
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('Example app listening on port 8080!');
 })
